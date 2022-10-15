7540b6a8be941bba9fb1b4631eb6f0ea720ae8a5
chibatakashi <nixon0290211@icloud.com>
2022-10-15 14:09:57 +0900

    データの登録

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,23 +2,39 @@
 
 namespace App\Http\Controllers;
 
-// Memoクラスを読み込む
 use App\Models\Memo;
 use Illuminate\Http\Request;
 
-
 class MemoController extends Controller
 {
-    // indexページへ移動
     public function index()
     {
-        // モデル名::テーブル全件取得
         $memos = Memo::all();
-        // memosティレクトリーの中のindexページを指定し、memosの連想配列を代入
         return view('memos.index', ['memos' => $memos]);
     }
 
-    // showページへ移動
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+    // インスタンスの作成
+    $memo = new Memo;
+
+    // 値の用意
+    $memo->title = $request->title;
+    $memo->body = $request->body;
+
+    // インスタンスに値を設定して保存
+    $memo->save();
+
+    // 登録したらindexに戻る
+    return redirect('/memos');
+    }
+
+
     public function show($id)
     {
         $memo = Memo::find($id);
