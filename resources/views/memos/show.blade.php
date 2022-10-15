469fb165b83eef5015c670d4b921e5fff5e5e189
chibatakashi <nixon0290211@icloud.com>
2022-10-15 15:11:39 +0900

    データの更新、削除

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>memo index</title>
+    <title>memo show</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <h1>タイトル一覧</h1>
-    <ul>
-        @foreach ($memos as $memo)
-            <!-- // リンク先をidで取得し名前で出力 -->
-            <li><a href="/memos/{{ $memo->id }}">{{ $memo->title }}</a></li>
-        @endforeach
-    </ul>
+    <a href="/memos">戻る</a>
+    <h1>{{ $memo->title }}</h1>
+    <p>{!! nl2br(e($memo->body)) !!}</p>
+    <div class="button-group">
+        <!-- $memoのidを元に編集ページへ遷移する -->
+        <button onclick="location.href='/memos/{{ $memo->id }}/edit'">編集する</button>
+        <form action="/memos/{{ $memo->id }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
 </body>
-</html> 
+</html>
